506af39fbbdf247907009bace95e27a991c7529d
Moesleazy <76605558+Moesleazy@users.noreply.github.com>
2022-09-01 10:16:29 -0400

    Add files via upload

---
+++ constants/data.js
@@ -0,0 +1,60 @@
+import './image';
+
+const pizza = [
+  {
+    title: 'Ultimate Supreme Pizza',
+    price: '$18.99',
+    tags: 'Pepperoni | Sausage | Ham | Beef | Onion | Green Pepper | Tomato | Black & Greem Olive | Mushroom | Banana Pepper',
+  },
+  {
+    title: 'Chicken Pesto Pizza ',
+    price: '$15.99',
+    tags: 'Grilled Chicken | Tomato | Fresh Pesto',
+  },
+  {
+    title: 'Meat Lover Pizza',
+    price: '$15.99',
+    tags: 'Pepperoni | Sausage | Bacon | Beef | Ham',
+  },
+  {
+    title: 'Margarita Pizza',
+    price: '$15.99',
+    tags: 'Tomato | Fresh Basil topped over Olive Oil',
+  },
+  {
+    title: 'Chicken Alfredo Pizza',
+    price: '$15.99',
+    tags: 'Alfredo Sauce | Grilled Chicken | Mushrooms | Black Olives',
+  },
+];
+
+const calzone = [
+  {
+    title: 'Spinach Calzone',
+    price: '$10.99',
+    tags: 'Spinach | Fresh Garlic | Ricotta | Parmeasan',
+  },
+  {
+    title: "Veggie Calzone",
+    price: '$10.99',
+    tags: 'Spinach | Garlic Ricotta | Parmesan | Provolone | Mozzarella',
+  },
+  {
+    title: 'Hawaiian Calzone',
+    price: '$10.99',
+    tags: 'Cheese | Ham | Pineapple',
+  },
+  {
+    title: 'Sicilian Stromboli',
+    price: '$12.99',
+    tags: 'Pepperoni | Sausage | Onions | Green Pepper | Mushrooms | Provolone',
+  },
+  {
+    title: 'Four Cheese Calzone',
+    price: '$12.99',
+    tags: 'Mozzarella | Provolone | Ricotta | Parmeasan',
+  },
+];
+
+
+export default { pizza, calzone };
---
+++ constants/image.js
@@ -0,0 +1,27 @@
+import bg from '../assets/bg.png';
+import gallery01 from '../assets/gallery01.jpg';
+import gallery02 from '../assets/gallery02.jpg';
+import gallery03 from '../assets/gallery03.jpg';
+import gallery04 from '../assets/gallery04.jpg';
+import knife from '../assets/knife.png';
+import menu from '../assets/menu.png';
+import spoon from '../assets/spoon.svg';
+import welcome from '../assets/welcome.png';
+import findus from '../assets/findus.png';
+import pizza from "../assets/pizza.png";
+import p from "../assets/P.png";
+
+export default {
+  bg,
+  gallery01,
+  gallery02,
+  gallery03,
+  gallery04,
+  knife,
+  menu,
+  spoon,
+  welcome,
+  findus,
+  pizza,
+  p,
+};
